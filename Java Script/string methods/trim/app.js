7f19944c23d80ba04a3c0715d33a76b3d1016fb2
Ishan0011 <igfocus222@gmail.com>
2026-09-19 20:31:58 +0530

    trim

---
+++ Java Script/string methods/trim/app.js
@@ -0,0 +1,5 @@
+let mes = "  Hello World  ";
+console.log(mes.trim());  // removes whitespace from both ends of a string
+console.log(mes.trimStart());  // removes whitespace from the start of a string
+console.log(mes.trimEnd());  // removes whitespace from the end of a string     
+console.log(mes); // original string remains unchanged
