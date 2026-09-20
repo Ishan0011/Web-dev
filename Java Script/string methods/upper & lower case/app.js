92cac2398b20c0fe7e5780903a411377acf6b5a2
Ishan0011 <igfocus222@gmail.com>
2026-09-20 20:51:36 +0530

    case

---
+++ Java Script/string methods/upper & lower case/app.js
@@ -0,0 +1,3 @@
+let mes = "Hello World";
+console.log(mes.toUpperCase()); // converts all characters in a string to uppercase
+console.log(mes.toLowerCase()); // converts all characters in a string to lowercase 
