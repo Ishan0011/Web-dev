669809d690da4c5f3138624cc8cd6af8ad883a45
Ishan0011 <igfocus222@gmail.com>
2026-09-21 19:33:51 +0530

    changer

--- Java Script/string methods/upper & lower case/app.js
+++ Java Script/string methods/upper & lower case/app.js
@@ -1,3 +1,4 @@
 let mes = "Hello World";
 console.log(mes.toUpperCase()); // converts all characters in a string to uppercase
 console.log(mes.toLowerCase()); // converts all characters in a string to lowercase 
+console.log(mes.charAt(0).toUpperCase) // converts the first character of a string to uppercase
