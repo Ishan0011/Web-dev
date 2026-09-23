27aa4b81516f6953625c805e9d673f568ae43f22
Ishan0011 <igfocus222@gmail.com>
2026-09-23 21:25:00 +0530

    prac

--- Java Script/practice/app.js
+++ Java Script/practice/app.js
@@ -1,16 +1,1 @@
-let color ="green";
 
-switch(color){
-    case "red":
-        console.log("stop");
-        break;  
-    case "yellow":
-        console.log("wait");
-        break;
-    case "green":
-        console.log("go");
-        break;
-    default:
-        console.log("invalid color");
-
-}
